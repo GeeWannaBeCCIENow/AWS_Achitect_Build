7759e25cee09db47be3e9f9a23776de31109b024
GeeWannaBeCCIENow <ggseide@gmail.com>
2025-06-01 10:33:25 -0400

    Update eip.tf

--- eip.tf
+++ eip.tf
@@ -8,12 +8,12 @@ resource "aws_eip" "eip" {
 }
 
 # A NAT gateway for private subnets: For ec2 instances softwares updates)
-resource "aws_nat_gateway" "nat_for_private_subnet" {
+resource "aws_nat_gateway" "nat_private_subnet" {
   allocation_id = aws_eip.eip.id
   subnet_id     = aws_subnet.public_subnet_1.id 
 
   tags = {
-    Name = "NAT for private subnet"
+    Name = "NAT_private_subnet"
   }
 
   depends_on = [aws_internet_gateway.IGW]
@@ -25,7 +25,7 @@ resource "aws_route_table" "rt_private" {
 
   route {
     cidr_block     = "0.0.0.0/0"
-    nat_gateway_id = aws_nat_gateway.nat_for_private_subnet.id
+    nat_gateway_id = aws_nat_gateway.nat_private_subnet.id
   }
 }
 
