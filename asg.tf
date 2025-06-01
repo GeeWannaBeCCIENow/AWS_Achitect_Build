0a555fae7912c55fcad630db1b57045fd194c9d3
GeeWannaBeCCIENow <ggseide@gmail.com>
2025-06-01 10:31:02 -0400

    Update asg.tf

--- asg.tf
+++ asg.tf
@@ -8,7 +8,7 @@ resource "aws_launch_template" "ec2_launch_templ" {
   network_interfaces {
     associate_public_ip_address = false
     subnet_id                   = aws_subnet.private_subnet_1.id
-    security_groups             = [aws_security_group.sg_for_ec2.id]
+    security_groups             = [aws_security_group.ec2_sg.id]
   }
   tag_specifications {
     resource_type = "instance"
